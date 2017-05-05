80d333e3a4b70690c8f9521fe2b68c7d7f363fe6
wteam-xq <857609086@qq.com>
2017-05-05 15:18:19 +0800

    update UI

--- routes/admin.js
+++ routes/admin.js
@@ -48,17 +48,19 @@ router.post('/upload/ico', tkdControl.uploadIco);
 /*个人简历后台列表页*/
 router.get('/resume', resumeControl.resumeIndex);
 router.get('/resume/index', resumeControl.resumeIndex);
+
 /*git_api后台列表*/
 router.get('/git_api', gitApiControl.gitApiIndex);
 router.get('/git_api/index', gitApiControl.gitApiIndex);
+
 /* 用户组后台列表页 */
 router.get('/users', userCtrol.userList);
 // 查询用户(暂根据邮箱查询)
 router.get('/user/search', userCtrol.searchUser);
-// 添加用户. 
+// 添加用户.
 router.route('/user/add')
 .post(userCtrol.addUserPost);
-// 更新用户 
+// 更新用户
 router.route('/user/update')
 .get(userCtrol.updateUser)
 .post(userCtrol.updateUserPost);
